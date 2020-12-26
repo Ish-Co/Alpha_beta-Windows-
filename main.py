7adb50f0313b4194233e7030bcbc3b98500f9c20
Ismail Sghir <73454465+Ish-Co@users.noreply.github.com>
2020-12-26 18:35:07 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import config_tastbar            #This import method will compile the "config" before executing the line below.
+import config_cursor_pos as cursor
+import datetime
+import turtle
+
+#Hide taskbar before starting the GUI
+config_tastbar.hide_taskbar()
+
+#Creating an instance of the class
+root = tk.Tk()
+
+#Collecting the screen resolution through tkinter and assign it to Vars
+width = root.winfo_screenwidth()
+height = root.winfo_screenheight()
+
+#Setting the title of the window
+root.title("Alpha v1.0")
+
+#Disable resizing the GUI as (x = false, y = false)
+root.resizable(1,1)
+
+#Disable minimize
+root.wm_minsize(0,0)
+
+root.configure(bg = "black")
+
+#Setting the resolution of the GUI using the Vars.
+root.geometry('%dx%d+0+0' %(width,height))
